e521c6dfa461311bb1f7773276c286f7c82d52ac
Erik Thompson <erik.james.thompson@gmail.com>
2019-11-23 12:21:34 -0800

    gh pages addition

---
+++ build/precache-manifest.d56fcec991e6e54caa27d5fab061083d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaa102ab77ac596d15df199fd8f9dfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba7e02152b1849c00bf2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fb0c9ca5cbd80fe2a91",
+    "url": "/static/js/2.e1806175.chunk.js"
+  },
+  {
+    "revision": "ba7e02152b1849c00bf2",
+    "url": "/static/js/main.8df7facf.chunk.js"
+  },
+  {
+    "revision": "8cf8c50bb32b0e821bf9",
+    "url": "/static/js/runtime-main.a06b4461.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
